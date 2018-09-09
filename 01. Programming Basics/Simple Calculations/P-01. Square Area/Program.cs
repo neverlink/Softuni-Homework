21fc63f31e1ad252cadad9c00decd56664804f6a
Neverlink <neverlink1337@gmail.com>
2018-09-09 13:05:47 +0300

    Add recent homework "Simple Calculations"

---
+++ 01. Programming Basics/Simple Calculations/P-01. Square Area/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_01.Square_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int area = sideA * sideA;
+            Console.WriteLine(area);
+        }
+    }
+}
