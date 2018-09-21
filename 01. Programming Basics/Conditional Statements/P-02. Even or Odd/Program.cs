d2b7dffcbcd1d69610251be7e699736d6ec06d02
Neverlink <neverlink1337@gmail.com>
2018-09-21 21:55:14 +0300

    Append homework

---
+++ 01. Programming Basics/Conditional Statements/P-02. Even or Odd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_02.Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
