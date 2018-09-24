94357d758108604f24d4b247a39b181e3309ef63
Neverlink <neverlink1337@gmail.com>
2018-09-24 17:57:16 +0300

    More homework

---
+++ 01. Programming Basics/05 - Conditional Statements - Exercise/P-05. Time + 15 Minutes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_05.Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes = minutes - 60;
+            }
+            if (hours >= 24)
+            {
+                hours = 0;
+            }
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
