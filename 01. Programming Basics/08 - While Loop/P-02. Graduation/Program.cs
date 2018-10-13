37fd2acbdddba9b0e8618b63ce98ca604b3e4f46
Neverlink <neverlink1337@gmail.com>
2018-10-13 19:33:22 +0300

    Add homework "While Loop"

---
+++ 01. Programming Basics/08 - While Loop/P-02. Graduation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_02.Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            int counter = 12;
+            double gradeSum = 0;
+
+            while (counter > 0)
+            {
+                double currentGrade = double.Parse(Console.ReadLine());
+                if (currentGrade >= 4)
+                {
+                    gradeSum += currentGrade;
+                    counter--;
+                }
+            }
+            double averageGrade = gradeSum / 12;
+            Console.WriteLine($"{studentName} graduated. Average grade: {averageGrade:F2}");
+        }
+    }
+}
---
+++ 01. Programming Basics/08 - While Loop/P-07. Water dispenser/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_07.Water_dispenser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cupAvailableCapacity = int.Parse(Console.ReadLine());
+            int tapCount = 0;
+
+            while (cupAvailableCapacity > 0)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "Easy":
+                        cupAvailableCapacity -= 50;
+                        break;
+                    case "Medium":
+                        cupAvailableCapacity -= 100;
+                        break;
+                    case "Hard":
+                        cupAvailableCapacity -= 200;
+                        break;
+                    default:
+                        break;
+                }
+                tapCount++;
+                if (cupAvailableCapacity < 0)
+                {
+                    Console.WriteLine($"{Math.Abs(cupAvailableCapacity)}ml has been spilled.");
+                    break;
+                }
+                else if (cupAvailableCapacity == 0)
+                {
+                    Console.WriteLine($"The dispenser has been tapped {tapCount} times.");
+                    break;
+                }
+            }
+        }
+    }
+}
