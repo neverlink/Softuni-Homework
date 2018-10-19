35f954ad002529214958dcf2800c014ef1476aa7
Neverlink <neverlink1337@gmail.com>
2018-10-19 16:40:22 +0300

    Add homework "For-Loop - Exercise"

---
+++ 01. Programming Basics/11 - For-Loop - Exercise/P-04. Equal Pairs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_04.Equal_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int diff = 0;
+            int lastSum = 0;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                int firstNumber = int.Parse(Console.ReadLine());
+                int secondNumber = int.Parse(Console.ReadLine());
+                sum = firstNumber + secondNumber;
+                if (i > 0)
+                {
+                    diff = Math.Abs(lastSum - sum);
+                }
+                lastSum = sum;
+            }
+            if (diff == 0)
+            {
+                Console.WriteLine($"Yes, value={lastSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, maxdiff = {diff}");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/11 - For-Loop - Exercise/P-07. Salary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_07.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+            for (int i = 0; i < tabs; i++)
+            {
+                string website = Console.ReadLine();
+                switch (website)
+                {
+                    case "Facebook":
+                        salary -= 150.00;
+                        break;
+                    case "Instagram":
+                        salary -= 100.00;
+                        break;
+                    case "Reddit":
+                        salary -= 50.00;
+                        break;
+                    default:
+                        break;
+                }
+                if (salary <= 0.00)
+                {
+                    break;
+                }
+            }
+            if (salary <= 0.00)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
