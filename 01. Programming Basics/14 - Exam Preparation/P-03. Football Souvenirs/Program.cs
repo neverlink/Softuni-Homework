6789c3f88df7884ecfe65bedd14e6367bfccb99e
Neverlink <neverlink1337@gmail.com>
2018-11-03 14:10:51 +0200

    Add homework "Exam Preparation"

---
+++ 01. Programming Basics/14 - Exam Preparation/P-03. Football Souvenirs/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_03.Football_Souvenirs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string teamName = Console.ReadLine();
+            string souvenirType = Console.ReadLine();
+            int souvenirCount = int.Parse(Console.ReadLine());
+            double souvenirPrice = 0;
+            bool invalidCountry = false;
+            bool invalidStock = false;
+
+            switch (teamName)
+            {
+                case "Argentina":
+                    switch (souvenirType)
+                    {
+                        case "flags":
+                            souvenirPrice = 3.25;
+                            break;
+                        case "caps":
+                            souvenirPrice = 7.20;
+                            break;
+                        case "posters":
+                            souvenirPrice = 5.10;
+                            break;
+                        case "stickers":
+                            souvenirPrice = 1.25;
+                            break;
+                        default:
+                            invalidStock = true;
+                            break;
+                    }
+                    break;
+                case "Brazil":
+                    switch (souvenirType)
+                    {
+                        case "flags":
+                            souvenirPrice = 4.20;
+                            break;
+                        case "caps":
+                            souvenirPrice = 8.50;
+                            break;
+                        case "posters":
+                            souvenirPrice = 5.35;
+                            break;
+                        case "stickers":
+                            souvenirPrice = 1.20;
+                            break;
+                        default:
+                            invalidStock = true;
+                            break;
+                    }
+                    break;
+                case "Croatia":
+                    switch (souvenirType)
+                    {
+                        case "flags":
+                            souvenirPrice = 2.75;
+                            break;
+                        case "caps":
+                            souvenirPrice = 6.90;
+                            break;
+                        case "posters":
+                            souvenirPrice = 4.95;
+                            break;
+                        case "stickers":
+                            souvenirPrice = 1.10;
+                            break;
+                        default:
+                            invalidStock = true;
+                            break;
+                    }
+                    break;
+                case "Denmark":
+                    switch (souvenirType)
+                    {
+                        case "flags":
+                            souvenirPrice = 3.10;
+                            break;
+                        case "caps":
+                            souvenirPrice = 6.50;
+                            break;
+                        case "posters":
+                            souvenirPrice = 4.80;
+                            break;
+                        case "stickers":
+                            souvenirPrice = 0.90;
+                            break;
+                        default:
+                            invalidStock = true;
+                            break;
+                    }
+                    break;
+                default:
+                    invalidCountry = true;
+                    break;
+            }
+            double total = souvenirPrice * souvenirCount;
+            if (invalidCountry)
+            {
+                Console.WriteLine("Invalid country!");
+            }
+            else if (invalidStock)
+            {
+                Console.WriteLine("Invalid stock!");
+            }
+            else
+            {
+                Console.WriteLine($"Pepi bought {souvenirCount} {souvenirType} of {teamName} for {total:F2} lv.") ;
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/14 - Exam Preparation/P-05. Fan Shop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_05.Fan_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+            int total = 0;
+            for (int i = 0; i < count; i++)
+            {
+                string item = Console.ReadLine();
+                switch (item)
+                {
+                    case "hoodie":
+                        total += 30;
+                        break;
+                    case "keychain":
+                        total += 4;
+                        break;
+                    case "T-shirt":
+                        total += 20;
+                        break;
+                    case "flag":
+                        total += 15;
+                        break;
+                    case "sticker":
+                        total += 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (budget >= total)
+            {
+                Console.WriteLine($"You bought {count} items and left with {budget - total} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {total - budget} more lv.");
+            }
+        }
+    }
+}
