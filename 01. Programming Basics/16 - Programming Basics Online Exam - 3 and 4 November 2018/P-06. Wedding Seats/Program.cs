a9b54a25bb537b7e4a4f60b7e18d093e4c1572b4
Neverlink <neverlink1337@gmail.com>
2018-11-05 18:17:57 +0200

    Add "Online Exam - 3 and 4 November 2018"

---
+++ 01. Programming Basics/16 - Programming Basics Online Exam - 3 and 4 November 2018/P-06. Wedding Seats/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_06.Wedding_Seats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char lastSector = char.Parse(Console.ReadLine());
+            int rowsPerSector = int.Parse(Console.ReadLine());
+            int seatsPerOddSector = int.Parse(Console.ReadLine());
+            int seatsPerEvenSector = seatsPerOddSector + 2;
+            int count = 0;
+
+            for (char i = 'A'; i <= lastSector; i++)
+            {
+                for (int j = 1; j <= rowsPerSector; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        for (char k = 'a'; k < 'a' + seatsPerEvenSector; k++)
+                        {
+                            Console.WriteLine($"{i}{j}{k}");
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        for (char k = 'a'; k < 'a' + seatsPerOddSector; k++)
+                        {
+                            Console.WriteLine($"{i}{j}{k}");
+                            count++;
+                        }
+                    }
+                }
+                rowsPerSector++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
